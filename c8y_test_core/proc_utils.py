31cff9283e84892731f93fae19011965daf3ebe0
Reuben Miller <reuben.d.miller@gmail.com>
2022-12-17 09:49:26 +0100

    feat: add realtime subscribers
    Realtime subscribers require `go-c8y-cli` to be installed.

---
+++ c8y_test_core/proc_utils.py
@@ -0,0 +1,36 @@
+"""json reader"""
+import json
+import subprocess
+from typing import Any, Callable, List, Optional
+
+
+class JsonReader:
+    """JSON reader supports parsing stdout and returning a list
+    using the preferred class factory, or by default a list of dictionaries
+    """
+
+    def __init__(self, proc: subprocess.Popen) -> None:
+        self._proc = proc
+
+    def wait(self, timeout: float = None):
+        """Wait for the process to finish
+
+        Args:
+            timeout (float, optional): Timeout in seconds. Defaults to None.
+        """
+        code = self._proc.wait(timeout)
+        assert code == 0
+
+    def read_all(self, func: Optional[Callable] = None) -> Optional[List[Any]]:
+        """Read all data and transform the output using a given function
+
+        Args:
+            func (Optional[Callable], optional): Function to be called on each outupt line.
+                Defaults to None.
+
+        Returns:
+            Optional[List[Any]]: List of objects created from each line of output
+        """
+        if func:
+            return [func(json.loads(line)) for line in self._proc.stdout]
+        return [json.loads(line) for line in self._proc.stdout]
---
+++ c8y_test_core/realtime.py
@@ -0,0 +1,97 @@
+"""c8y realtime utilities
+The functions here rely on the cli tool go-c8y-cli!
+"""
+import subprocess
+from c8y_test_core.proc_utils import JsonReader
+
+
+class Subscriber:
+    """Subscriber factory"""
+
+    @classmethod
+    def _subscribe(cls, typename: str, device_id: str, duration: int) -> JsonReader:
+        # pylint: disable=consider-using-with
+        proc = subprocess.Popen(
+            [
+                "c8y",
+                "measurements",
+                "subscribe",
+                "--device",
+                device_id,
+                "--duration",
+                f"{duration}s",
+            ],
+            universal_newlines=True,
+            stdout=subprocess.PIPE,
+        )
+        return JsonReader(proc)
+
+    # pylint: disable=too-few-public-methods
+    @classmethod
+    def to_measurements(cls, device_id: str, duration: int) -> JsonReader:
+        """Create a subscription to measurements for a device
+
+        Args:
+            device_id (str): device id to subscribe to
+            duration (int): Duration in seconds to subscribe for
+
+        Returns:
+            JsonReader: Reader which can be called to access the data
+        """
+        return cls._subscribe("measurements", device_id, duration)
+
+    @classmethod
+    def to_events(cls, device_id: str, duration: int) -> JsonReader:
+        """Create a subscription to events for a device
+
+        Args:
+            device_id (str): device id to subscribe to
+            duration (int): Duration in seconds to subscribe for
+
+        Returns:
+            JsonReader: Reader which can be called to access the data
+        """
+        # pylint: disable=consider-using-with
+        return cls._subscribe("events", device_id, duration)
+
+    @classmethod
+    def to_alarms(cls, device_id: str, duration: int) -> JsonReader:
+        """Create a subscription to alarms for a device
+
+        Args:
+            device_id (str): device id to subscribe to
+            duration (int): Duration in seconds to subscribe for
+
+        Returns:
+            JsonReader: Reader which can be called to access the data
+        """
+        # pylint: disable=consider-using-with
+        return cls._subscribe("alarms", device_id, duration)
+
+    @classmethod
+    def to_operations(cls, device_id: str, duration: int) -> JsonReader:
+        """Create a subscription to operations for a device
+
+        Args:
+            device_id (str): device id to subscribe to
+            duration (int): Duration in seconds to subscribe for
+
+        Returns:
+            JsonReader: Reader which can be called to access the data
+        """
+        # pylint: disable=consider-using-with
+        return cls._subscribe("operations", device_id, duration)
+
+    @classmethod
+    def to_inventory(cls, device_id: str, duration: int) -> JsonReader:
+        """Create a subscription to managedobjects/inventory for a device
+
+        Args:
+            device_id (str): device id to subscribe to
+            duration (int): Duration in seconds to subscribe for
+
+        Returns:
+            JsonReader: Reader which can be called to access the data
+        """
+        # pylint: disable=consider-using-with
+        return cls._subscribe("inventory", device_id, duration)
