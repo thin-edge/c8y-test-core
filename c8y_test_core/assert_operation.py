1b803bc3cac001a26c512b44f2dbac4f226c6818
Reuben Miller <reuben.d.miller@gmail.com>
2023-06-29 11:38:20 +0200

    fix assertion mixup

--- c8y_test_core/assert_operation.py
+++ c8y_test_core/assert_operation.py
@@ -101,8 +101,12 @@ class AssertOperation:
     def assert_not_done(self, **kwargs) -> Operation:
         """Assert that the operation status to be not done (e.g. SUCCESSFUL or FAILED)"""
         self.fetch_operation()
-        assert self.operation.status != Operation.Status.PENDING, (
-            f"Expected operation (id={self.operation.id}) to not be {Operation.Status.PENDING}, "
+        assert self.operation.status not in [
+            Operation.Status.SUCCESSFUL,
+            Operation.Status.FAILED,
+        ](
+            f"Expected operation (id={self.operation.id}) to not be done "
+            f"[{Operation.Status.SUCCESSFUL} or {Operation.Status.FAILED}]), "
             f"but got: {self.operation.status}"
         )
         return self.operation
@@ -110,12 +114,8 @@ class AssertOperation:
     def assert_not_pending(self, **kwargs) -> Operation:
         """Assert that the operation status to be not PENDING"""
         self.fetch_operation()
-        assert self.operation.status not in [
-            Operation.Status.SUCCESSFUL,
-            Operation.Status.FAILED,
-        ](
-            f"Expected operation (id={self.operation.id}) to not be done "
-            f"[{Operation.Status.SUCCESSFUL} or {Operation.Status.FAILED}]), "
+        assert self.operation.status != Operation.Status.PENDING, (
+            f"Expected operation (id={self.operation.id}) to not be {Operation.Status.PENDING}, "
             f"but got: {self.operation.status}"
         )
         return self.operation
